a29b728af852ca515aceeb9ef0eec7e5e4566164
Antoine SARRAZIN <tony78.itouch@gmail.com>
2018-12-01 16:06:49 +0100

    First Commit
    - Start of Handpan instrument
    - Basic Movement in space
    - Start of LD Ingredients

---
+++ LD43/Assets/Scripts/InstrumentManager.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstrumentManager : MonoBehaviour {
+
+    public bool showDrum = false;
+
+    public GameObject lh;
+    public GameObject rh;
+    public GameObject drum;
+
+    public float angleVariation = 60f;
+    public float lerpValue = .5f;
+    public float showOffset = 300f;
+    public float showLerp = .5f;
+
+    private Vector3 baseLhRot;
+    private Vector3 baseRhRot;
+    private Vector3 drumBasePosition;
+
+    private float targetLRot;
+    private float targetRRot;
+
+    // Use this for initialization
+    void Start () {
+        baseLhRot = lh.transform.rotation.eulerAngles;
+        baseRhRot = rh.transform.rotation.eulerAngles;
+        drumBasePosition = drum.transform.position;
+
+        ShowDrum(showDrum);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        ShowDrum(showDrum);
+        PlayInstrument();
+        
+	}
+
+    private void PlayInstrument()
+    {
+        int leftNote = GetCurrentNote(Input.GetAxis("LT"));
+        int rightNote = GetCurrentNote(Input.GetAxis("RT"));
+
+        targetLRot = Mathf.Lerp(targetLRot, baseLhRot.z+(leftNote * -angleVariation), lerpValue);
+        lh.transform.eulerAngles = new Vector3(0, 0, targetLRot);
+
+        targetRRot = Mathf.Lerp(targetRRot, baseRhRot.z + (rightNote * angleVariation), lerpValue);
+        rh.transform.eulerAngles = new Vector3(0, 0, targetRRot);
+
+        
+    }
+
+    private int GetCurrentNote(float axis)
+    {
+        if (axis > .8f)
+        {
+            return 2;
+        }
+        else if (axis >= .3f && axis <= .8f)
+        {
+            return 1;
+        } else
+        {
+            return 0;
+        }
+    }
+
+    private void ShowDrum(bool show)
+    {
+        if (show == true)
+        {
+            drum.transform.position = Vector3.Lerp(drum.transform.position, new Vector3 (drumBasePosition.x, drumBasePosition.y, drumBasePosition.z), showLerp);
+        }
+        
+        if (show == false)
+        {
+            drum.transform.position = Vector3.Lerp(drum.transform.position, new Vector3(drumBasePosition.x, drumBasePosition.y - showOffset, drumBasePosition.z), showLerp);
+        }
+
+    }
+
+    private void PlayNote(int note)
+    {
+
+    }
+}
---
+++ LD43/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        } else
+        {
+            moveDirection.x = Input.GetAxis("Horizontal") * speed;
+            moveDirection.z = Input.GetAxis("Vertical") * speed;
+            moveDirection = transform.TransformDirection(moveDirection);
+        }
+
+        // Apply gravity
+        moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
